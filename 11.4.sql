b5ff4a64a389d7e9af8088d8fbd8e6eefdc65074
a627523830 <53019362+a627523830@users.noreply.github.com>
2019-07-25 17:05:01 -0400

    Add files via upload

---
+++ 11.4.sql
@@ -0,0 +1,26 @@
+SELECT
+    trip_id
+FROM
+    trips
+WHERE 
+    duration>500
+ORDER BY duration DESC
+
+
+SELECT
+	*
+FROM
+	stations
+WHERE
+	station_id=84
+	
+Select
+	mintemperaturef,
+	zip
+FROM
+	weather
+WHERE
+	zip=94301
+	
+
+	
